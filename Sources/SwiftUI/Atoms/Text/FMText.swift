5b3b0583ae33e4f5c9ace28dac9a095e0faaf84a
fummicc1 <fumiya.tennis1@gmail.com>
2022-04-16 11:09:57 +0900

    Add FMLabel

---
+++ Sources/SwiftUI/Atoms/Text/FMText.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct FMText: View {
+
+    public init(
+        text: Binding<String>,
+        font: FMFont,
+        padding: EdgeInsets = .init(
+            top: 8,
+            leading: 12,
+            bottom: 8,
+            trailing: 12
+        )
+    ) {
+        self._text = text
+        self.font = font
+        self.padding = padding
+    }
+
+
+    @Binding public var text: String
+    public var font: FMFont
+    public var padding: EdgeInsets
+
+    var body: some View {
+        Text(text)
+            .modifier(FMFontModifier(font: font))
+            .padding(padding)
+    }
+}
+
+struct FMText_Previews: PreviewProvider {
+    static var previews: some View {
+        FMText(
+            text: .constant("Hello World"),
+            font: .h1
+        )
+        .previewLayout(.sizeThatFits)
+    }
+}
